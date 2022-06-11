0cd8581c80a5a1d13355b4790f6cdb517fd8a2a6
HugoRibeiroIPS <95934130+HugoRibeiroIPS@users.noreply.github.com>
2022-06-11 18:43:26 +0100

    Criada a tabela de itens do leilão e barra de pesquisa para a mesma

--- auctions.js
+++ auctions.js
@@ -1,1 +1,62 @@
-//
+const url_api = "https://api.hypixel.net/skyblock/auctions";
+  
+async function getapi(url) 
+{
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    if (response) 
+    {
+        hideloader();
+    }
+    show(data);
+}
+getapi(url_api);
+
+function hideloader() 
+{
+    document.getElementById('loading').style.display = 'none';
+}
+function show(data) 
+{
+    let tab = 
+        `<tr>
+          <th>Nome do item</th>
+          <th>Lore do item</th>
+          <th>Tier do item</th>
+          <th>Aposta mais alta</th>
+         </tr>`;
+    
+    for (let a in data.auctions) 
+    {
+        tab += `<tr> 
+    <td>${data.auctions[a].item_name} </td>
+    <td>${data.auctions[a].item_lore}</td>
+    <td>${data.auctions[a].tier}</td> 
+    <td>${data.auctions[a].highest_bid_amount} coins</td>
+</tr>`;
+    }
+    document.getElementById("leilao").innerHTML = tab;
+}
+
+function searchAuctions() {
+    var input, filter, found, table, tr, td, i, j;
+    input = document.getElementById("pesquisa");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("leilao");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+            }
+        }
+        if (found) {
+            tr[i].style.display = "";
+            found = false;
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-//
+var a = Math.floor(Math.random() * 255);
+var b = Math.floor(Math.random() * 255);
+var c = Math.floor(Math.random() * 255);
+var d = Math.floor(Math.random() * 255);
+console.log(a + "." + b + "." + c + "." + d);
